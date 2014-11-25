f9d9e7cd43c3954258874e9b8f4f63eb30aaab2c
Justin Ribeiro <justin@justinribeiro.com>
2014-11-25 14:37:15 -0800

    Templating for custom theme

---
+++ taxresults.php
@@ -0,0 +1,6 @@
+<div class="post-inner">
+  <div class="post-header">
+      <h3 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3> 
+  </div> <!-- /post-header -->
+  <div class="clear"></div>
+</div>
